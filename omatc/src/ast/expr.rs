776ca9a31892fe72382a979865d8da7ad625cd31
Toni <buch.toni@outlook.com>
2023-08-10 14:18:28 +0200

    updating ast/exprs.rs

--- omatc/src/ast/expr.rs
+++ omatc/src/ast/expr.rs
@@ -1,3 +1,25 @@
-pub enum Expr {
+use crate::ast::token::Token;
 
+pub enum Expr {
+    Assing(Assing),
+    Binary(Binary),
+    Set(Set),
+}
+
+pub struct Assing {
+    name: Token,
+    _type: Token,
+    value: Expr,
+}
+
+pub struct Binary {
+    lhs: Expr,
+    op: Token,
+    rhs: Expr,
+}
+
+pub struct Set {
+    object: Box<Expr>,
+    name: Token,
+    value: Box<Expr>,
 }
