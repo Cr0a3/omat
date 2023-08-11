a3e7875d0aa8b3012eb631fef059b16f74468f18
Toni Graphics <buch.toni@outlook.com>
2023-08-11 12:25:09 +0200

    finishing 1.0 of opack

--- opack/src/arg/config_json.rs
+++ opack/src/arg/config_json.rs
@@ -1,23 +1,34 @@
 
 use serde_json::Value;
 
+use std::process;
 use std::fs::File;
 use std::io::Read;
+use crate::error::error;
 
 pub fn read_config_json(path: &str) -> Value{
-    let mut file = File::open(path)?;
-
+    let file = File::open(path);
     let mut content = String::new();
-    file.read_to_string(&mut content)?;
+
+    match file {
+        Ok(mut f) => {
+            f.read_to_string(&mut content).expect("err");
+        }
+        Err(e) => {
+            error::error("Et011", format!("could not open json config file: {}", e).as_str());
+        },
+    }
+
 
     //Deserializes json data struct
     let data = serde_json::from_str(content.as_str());
     match data {
         Ok(v) => {
-
+            v
         }
         Err(e) => {
-            
+            error::error("Et010", format!("error while deseralizing json data struct: {}", e).as_str());
+            process::exit(-1);
         }
     }
 }
