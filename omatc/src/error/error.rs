5f50ce637ebf0e9b150db30d8f1d2f07c3ddf75e
Toni Graphics <buch.toni@outlook.com>
2023-08-12 16:20:26 +0200

    adding error_fab functions wireframe

--- omatc/src/error/error.rs
+++ omatc/src/error/error.rs
@@ -1,12 +1,30 @@
 use colored::Colorize;
 
 pub struct error_fab {
-
+    ecode: String,
+    msg: String,
+    fmt_lines: Vec<String>,
 }
 
 impl error_fab {
-    pub fn new() -> Self {
-        error_fab {  }
+    pub fn new(_ecode: String, _msg: String) -> Self {
+        error_fab {  
+            ecode: _ecode,
+            msg: _msg,
+            fmt_lines: Vec::new(),
+        }
+    }
+
+    pub fn add_code_line(&self, line: String, display_line_no: bool, line_no: usize, add: bool) {
+
+    }
+
+    pub fn add_where(&self, line_no: usize, where_start: usize, where_length: usize, where_msg_b: bool, where_msg: String) {
+
+    }
+
+    pub fn add_arrow(&self, file: String, line: usize, where_start: usize) {
+        
     }
 
     pub fn print(&self) {
