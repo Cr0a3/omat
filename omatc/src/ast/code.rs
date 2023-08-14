00d35f1269bff1b89bb508bdc86d6ab400a719d2
Toni Graphics <buch.toni@outlook.com>
2023-08-14 15:48:42 +0200

    starting implementing code gen llvm stuff

--- omatc/src/ast/code.rs
+++ omatc/src/ast/code.rs
@@ -1,6 +1,15 @@
 use crate::ast::expr::Expr;
 use crate::args;
 
+use inkwell::OptimizationLevel;
+use inkwell::context::Context;
+use inkwell::module::Module;
+use inkwell::targets::{CodeModel, RelocMode, FileType, Target, TargetMachine, TargetTriple, InitializationConfig};
+use std::path::Path;
+use std::rc::Rc;
+
+const OUT_FILE_STDTYPE: FileType = FileType::Object;
+
 pub struct CodeGenerator {
     exprs: Vec<Expr>,
 
@@ -19,10 +28,40 @@ pub struct CodeGenerator {
 
     current: usize,
     start: usize,
+
+    //llvm
+    target: Target,
+    machine: TargetMachine,
+    context: Rc<Context>,
+    module: Module<'_>,
 }
 
 impl CodeGenerator {
     pub fn new(_exprs: Vec<Expr>, _args: args::Args) -> Self {
+        //init llvm
+        Target::initialize_x86(&InitializationConfig::default());
+
+        let target = Target::from_name("x86-64").unwrap();
+        
+        let opt = OptimizationLevel::Default;
+        let reloc = RelocMode::Default;
+        let model = CodeModel::Default;
+
+
+        let target_machine = target.create_target_machine(
+            &TargetTriple::create(&_args.target.clone()),
+            "x86-64",
+            "+avx2",
+            opt,
+            reloc,
+            model
+        )
+        .unwrap();
+
+        let context = Rc::new(Context::create());
+
+        let module  = context.create_module("module");
+
         CodeGenerator { 
             exprs: _exprs, 
             output: _args.output, 
@@ -38,6 +77,12 @@ impl CodeGenerator {
             dynamic_linking: _args.dynamic_linking, 
             current: 0,
             start: 0,
+
+            //llvm
+            target: target,
+            machine: target_machine,
+            context: context,
+            module: module,
         }
     }
 
@@ -56,5 +101,15 @@ impl CodeGenerator {
             
             self.gen_expr();
         }
+        let out_file_type: FileType;
+
+        if self.obj {
+            out_file_type = FileType::Object;
+        }
+        else {
+            out_file_type = OUT_FILE_STDTYPE;
+        }
+
+        self.machine.write_to_file(&self.module, out_file_type, Path::new(format!("{}.o", self.output).as_str())).is_ok();
     }
 }
